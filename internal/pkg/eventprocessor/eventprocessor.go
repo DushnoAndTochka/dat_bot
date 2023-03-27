c2fdfb3f2dca2a3a7eb0600b2dd61640d22bb117
artem-telnov <telnowtema@mail.ru>
2023-03-28 00:03:47 +0300

    Add all dialogs

--- internal/pkg/eventprocessor/eventprocessor.go
+++ internal/pkg/eventprocessor/eventprocessor.go
@@ -1,25 +1,18 @@
 package eventprocessor
 
 import (
+	"regexp"
+
 	"github.com/mymmrac/telego"
 	tu "github.com/mymmrac/telego/telegoutil"
 )
-
-var startKeyboard = tu.Keyboard(
-	tu.KeyboardRow(
-		tu.KeyboardButton("Хочу предложить задачу"),
-	),
-	tu.KeyboardRow(
-		tu.KeyboardButton("/help"),
-	),
-).WithResizeKeyboard().WithInputFieldPlaceholder("Select something")
 
 func ProcessStartComand(bot *telego.Bot, update telego.Update) {
 
 	message := tu.Message(
 		tu.ID(update.Message.Chat.ID),
 		"Привет. Чем могут быть полезен ?",
-	).WithReplyMarkup(startKeyboard)
+	)
 
 	_, _ = bot.SendMessage(message)
 }
@@ -29,7 +22,7 @@ func ProcessHelpComand(bot *telego.Bot, update telego.Update) {
 	message := tu.Message(
 		tu.ID(update.Message.Chat.ID),
 		"На данный момент я умею не так много. \n - Через меня можно предложить задачу на разбор\n - Посмотреть топ 5 желаемых задач от подписчиков.",
-	).WithReplyMarkup(startKeyboard)
+	)
 
 	_, _ = bot.SendMessage(message)
 }
@@ -37,22 +30,56 @@ func ProcessHelpComand(bot *telego.Bot, update telego.Update) {
 func ProcessNotSupportedComandsComand(bot *telego.Bot, update telego.Update) {
 	message := tu.Message(
 		tu.ID(update.Message.Chat.ID),
-		"Unknown command, use /start",
+		"Неизвестная команда, используй /help.",
 	).WithReplyMarkup(tu.ReplyKeyboardRemove())
 
 	_, _ = bot.SendMessage(message)
 }
 
 func ProcessGetLinkFromReply(bot *telego.Bot, update telego.Update) {
-	message := tu.Message(
-		tu.ID(update.Message.Chat.ID),
-		"Спасибо",
-	).WithReplyMarkup(startKeyboard)
+	answer := update.Message.Text
+	var leetcodeValidLink = regexp.MustCompile(`^https:\/\/leetcode.com\/problems\/[a-z\-]*\/?$`)
+
+	var message *telego.SendMessageParams
+	if leetcodeValidLink.MatchString(answer) {
+		message = tu.Message(
+			tu.ID(update.Message.Chat.ID),
+			"Спасибо",
+		)
+	} else {
+		message = tu.Message(
+			tu.ID(update.Message.Chat.ID),
+			"Представленный вами URL не принадлежит LeetCode или указывает не на проблему.\nПример корректной ссылки: https://leetcode.com/problems/two-sum.",
+		)
+	}
 
 	_, _ = bot.SendMessage(message)
 }
 
-func ProcessProposeTaskFromMessage(bot *telego.Bot, update telego.Update) {
+func ProcessShowAllProposeProblems(bot *telego.Bot, update telego.Update) {
+	// message := tu.Message(
+	// 	tu.ID(update.Message.Chat.ID),
+	// 	"Текущий Топ предложенных задач, выглядит следуюзим образом:\n",
+	// ).WithReplyMarkup(tu.ForceReply())
+
+	message := tu.Message(
+		tu.ID(update.Message.Chat.ID),
+		"На этой неделе еще никто не успел предложить задачу. Вы можете быть первым.",
+	)
+
+	_, _ = bot.SendMessage(message)
+}
+
+func ProcessShowMyProposeProblem(bot *telego.Bot, update telego.Update) {
+	message := tu.Message(
+		tu.ID(update.Message.Chat.ID),
+		"На этой неделе вы не предложили никакой задачи. Самое время это сделать.\n/propose_problem",
+	)
+
+	_, _ = bot.SendMessage(message)
+}
+
+func ProcessProposeProblemFromMessage(bot *telego.Bot, update telego.Update) {
 	message := tu.Message(
 		tu.ID(update.Message.Chat.ID),
 		"Укажите ссылку на задачу.",
